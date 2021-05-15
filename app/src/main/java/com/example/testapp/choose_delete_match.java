3e7e8b7982bc3ce1e6ec1258627f393e3d61d3da
Anastasia Mousa <anastasiamousa34@gmail.com>
2021-05-15 20:46:06 +0300

    xml fixes

--- app/src/main/java/com/example/testapp/choose_delete_match.java
+++ app/src/main/java/com/example/testapp/choose_delete_match.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link config_match#newInstance} factory method to
+ * Use the {@link choose_delete_match#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class config_match extends Fragment {
+public class choose_delete_match extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class config_match extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public config_match() {
+    public choose_delete_match() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class config_match extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment config_match.
+     * @return A new instance of fragment choose_delete_match.
      */
     // TODO: Rename and change types and number of parameters
-    public static config_match newInstance(String param1, String param2) {
-        config_match fragment = new config_match();
+    public static choose_delete_match newInstance(String param1, String param2) {
+        choose_delete_match fragment = new choose_delete_match();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class config_match extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.choose_config_match, container, false);
+        return inflater.inflate(R.layout.choose_delete_match, container, false);
     }
 }
